6cffda97d92a1dac4767efd5b4375abb1c7e4ea3
Tophe <bezedache29@gmail.com>
2022-09-05 01:04:30 +0200

    Changer le return des images du club + reticfié search department & search month

---
+++ app/Http/Requests/SearchDepartmentRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class SearchDepartmentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'department_code' => ['required'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'department_code.required' => 'Le code du department est obligatoire',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        $response = response()->json($validator->errors(), 422);
+
+        throw new HttpResponseException($response);
+    }
+}
